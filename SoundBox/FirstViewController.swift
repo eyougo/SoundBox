28feb67e00880b528fa0841a0f2f191a3c86e1c8
eyougo <meihongyuan@163.com>
2016-08-27 21:17:34 +0800

    Initial Commit

---
+++ SoundBox/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SoundBox
+//
+//  Created by Mei on 8/27/16.
+//  Copyright © 2016 Mei. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
